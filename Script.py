48f328cfaa5e5fd098563c98f19f3944caa3fdf6
Rizwan Haidar <47399482+rizwanhaidar@users.noreply.github.com>
2020-01-31 16:24:34 +0500

    Update Script.py

--- Script.py
+++ Script.py
@@ -56,3 +56,25 @@ print(areas[9])
 
 # Print out the area of the living room
 print(areas[5])
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Sum of kitchen and bedroom area: eat_sleep_area
+eat_sleep_area=areas[3]+areas[7]
+
+# Print the variable eat_sleep_area
+print(eat_sleep_area)
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Use slicing to create downstairs
+
+downstairs = areas[0:6]
+# Use slicing to create upstairs
+upstairs = areas[6:10]
+
+# Print out downstairs and upstairs
+print(downstairs)
+print(upstairs)
