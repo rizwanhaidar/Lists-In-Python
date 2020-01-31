250b3f283325305adefd010407a2b5d78ae07247
Rizwan Haidar <47399482+rizwanhaidar@users.noreply.github.com>
2020-01-31 16:06:34 +0500

    Update Script.py

--- Script.py
+++ Script.py
@@ -43,3 +43,16 @@ print(house)
 
 # Print out the type of house
 print(type(house))
+
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Print out second element from areas
+print(areas[1])
+
+# Print out last element from areas
+print(areas[9])
+
+# Print out the area of the living room
+print(areas[5])
